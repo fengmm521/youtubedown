98ea9bc5426c74a07c6383f3b5a366e982462764
woodcol <mage@woodcol.com>
2017-12-10 21:17:42 +0800

    fix bugs

--- youtubedownload.py
+++ youtubedownload.py
@@ -45,9 +45,10 @@ sys.setdefaultencoding( "utf-8" )
 
 def makeMoive(ptitle,videoType = '1080p',outpth = 'out'):
     cmd = '/usr/local/bin/ffmpeg ffmpeg -i "%s/%s.mp4" -i "audio/%s.mp4" -vcodec copy -acodec copy "%s/%s.mp4"'%(videoType,ptitle,ptitle,outpth,ptitle)
+    print cmd
     os.system(cmd)
 
-def downloadWithURL(pURL = 'https://www.youtube.com/watch?v=cmSbXsFE3l8'):
+def downloadWithURL(pURL = 'https://www.youtube.com/watch?v=cmSbXsFE3l8',outpth = 'out'):
     yt = YouTube(pURL)
     alllist = yt.streams.all()
     # for x in alllist:
@@ -57,21 +58,35 @@ def downloadWithURL(pURL = 'https://www.youtube.com/watch?v=cmSbXsFE3l8'):
     abr128k = yt.streams.filter(abr="128kbps", file_extension='mp4').first()
     # print p1080.player_config['args']['title'].encode('utf-8')
     # print abr128k.player_config['args']['title'].encode('utf-8')
+    title = ''
+    audiopth = ''
     if abr128k:
         # abr128k.player_config['args']['title'] = abr128k.player_config['args']['title'].encode('utf-8')
         abr128k.download('audio')
+        audiopth = 'audio/' + abr128k.player_config['args']['title'].encode('utf-8')
 
+    videopth = ''
     if p1080:
         print p1080
         # p1080.player_config['args']['title'] = p1080.player_config['args']['title'].encode('utf-8')
         p1080.download('1080p')
+        
+        videopth = '1080p/' + p1080.player_config['args']['title'].encode('utf-8')
 
-        makeMoive(p1080.player_config['args']['title'])
+        title = ''
+
+        makeMoive(p1080.player_config['args']['title'],'1080p',outpth)
+
     elif p720:
         # p720.player_config['args']['title'] = p1080.player_config['args']['title'].encode('utf-8')
         p720.download('720p')
-        makeMoive(p1080.player_config['args']['title'],'720p')
 
+        videopth = '720p/' + p720.player_config['args']['title'].encode('utf-8')
+
+        makeMoive(p720.player_config['args']['title'].encode('utf-8'),'720p',outpth)
+
+    os.remove(audiopth)
+    os.remove(videopth)
 
 def main(args):
     turl = ''
@@ -79,9 +94,17 @@ def main(args):
         turl = args[1]
         print turl
         downloadWithURL(turl)
+    elif len(args) == 3:
+        turl = args[1]
+        outpth = args[2]
+        print turl
+        print outpth
+        downloadWithURL(turl,outpth)
     else:
         print "未输入要下载的视频URL地址,参考下边样式输入参数来下载:\n"
         print 'python youtubedownload.py 要下载的视频地址'
+        print '或者:'
+        print 'python youtubedownload.py 要下载的视频地址 输出的视频目录'
 
 if __name__ == '__main__':
     main(sys.argv)
