ccb8ca34ab66e02454f882c28417cfc16d3b24be
woodcol <mage@woodcol.com>
2017-12-15 17:30:03 +0800

    工具安装说明

--- installlib.sh
+++ installlib.sh
@@ -1,4 +1,4 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install pip
 brew install ffmpeg
+sudo easy_install pip
 pip install pytube --user
